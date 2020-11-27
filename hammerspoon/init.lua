943993d5ed4fef118978dc09c3b4d1fe68e61cae
Rufo Sanchez <rufo@rufosanchez.com>
2020-11-27 15:01:30 -0600

    init.lua: initial synergy watcher code

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -7,30 +7,60 @@ function dockMover()
     return
   end
   displays = newDisplays
-
+  
   for screen, position in pairs(displays) do
     if position.x > 0 then
       dockOnBottom = true
     end
   end
-
+  
   if dockOnBottom then
-	hs.alert.show("move dock to bottom")
-	hs.osascript.applescript([[ tell application "System Events"
-			tell dock preferences
-				set properties to {screen edge: bottom, autohide: true}
-			end tell
-		end tell ]])
+    hs.alert.show("move dock to bottom")
+    hs.osascript.applescript([[ tell application "System Events"
+      tell dock preferences
+        set properties to {screen edge: bottom, autohide: true}
+      end tell
+    end tell ]])
   else
-  	hs.alert.show("move dock to right")
-	hs.osascript.applescript([[ tell application "System Events"
-		tell dock preferences
-			set properties to {screen edge: right, autohide: false}
-		end tell
-	end tell ]])
+    hs.alert.show("move dock to right")
+    hs.osascript.applescript([[ tell application "System Events"
+      tell dock preferences
+        set properties to {screen edge: right, autohide: false}
+      end tell
+    end tell ]])
   end
-end)
 
+  local shouldStartSynergy = false
+  
+  hs.timer.doAfter(1, function()
+    synergyStarter(shouldStartSynergy)
+  end)
+
+  if #displays == 1 then
+    if string.find(displays[1].name, "LG HDR 4K") then
+      for interface, ip in pairs(hs.network.addresses()) do
+        if string.find(ip, "192.168.212.") then
+          print("found ip")
+          hs.network.ping("192.168.212.117", 3, 0.2, 0.2, "any", function(obj, msg, sn, err)
+            print("pingCallback")
+            if msg == "receivedPacket" then
+              shouldStartSynergy = true
+            end
+          end)
+          break
+        end
+      end
+    end
+  end
+end
+
+function synergyStarter(shouldStartSynergy)
+  if shouldStartSynergy then
+    print("synergy should start")
+  else
+    print("synergy should end")
+  end
+end
 
 dockMoverWatcher = hs.screen.watcher.new(dockMover)
 dockMoverWatcher:start()

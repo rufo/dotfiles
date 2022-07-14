4f83564c365f4be98340cafafb2f9ef47a45b3d5
Rufo Sanchez <rufo@github.com>
2022-07-14 09:50:15 -0500

    plugins.lua: add packadd for slightly better bootstrapping

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,7 +1,9 @@
 local fn = vim.fn
 local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local packer_bootsrap
 if fn.empty(fn.glob(install_path)) > 0 then
   packer_bootstrap = fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.api.nvim_command('packadd packer.nvim')
 end
 
 vim.cmd([[

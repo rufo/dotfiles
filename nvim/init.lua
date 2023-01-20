28601a70653e808413d57d0425aadcd85041d45a
Rufo Sanchez <rufo@rufosanchez.com>
2023-01-20 13:18:22 -0600

    nvim: switch to lazy.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,7 +7,75 @@ if vim.env.USE_CLASSIC_VIMRC then
   return
 end
 
-require('bootstrap').setup()
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local PKGS = {
+  'nvim-lua/plenary.nvim';
+  {'rebelot/kanagawa.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd([[colorscheme kanagawa]])
+    end
+  };
+  {'nvim-treesitter/nvim-treesitter', build=function ()
+    vim.cmd('TSUpdate')
+  end};
+  'wincent/ferret';
+  'tpope/vim-repeat';
+  'tpope/vim-surround';
+  'tpope/vim-fugitive';
+  'tpope/vim-rails';
+  'kyazdani42/nvim-web-devicons';
+  'kyazdani42/nvim-tree.lua';
+  'numToStr/Comment.nvim';
+  {'junegunn/fzf', build='./install --bin'};
+  'ibhagwan/fzf-lua';
+  'lewis6991/gitsigns.nvim';
+  'windwp/nvim-autopairs';
+  'VonHeikemen/lsp-zero.nvim';
+  'jose-elias-alvarez/null-ls.nvim';
+  'neovim/nvim-lspconfig';
+  'williamboman/mason.nvim';
+  'williamboman/mason-lspconfig.nvim';
+
+  -- Autocompletion
+  'hrsh7th/nvim-cmp';
+  'hrsh7th/cmp-buffer';
+  'hrsh7th/cmp-path';
+  'saadparwaiz1/cmp_luasnip';
+  'hrsh7th/cmp-nvim-lsp';
+  'hrsh7th/cmp-nvim-lua';
+  'hrsh7th/cmp-cmdline';
+
+  -- Snippets
+  'L3MON4D3/LuaSnip';
+  'rafamadriz/friendly-snippets';
+
+  'RRethy/nvim-treesitter-endwise';
+
+  'lukas-reineke/indent-blankline.nvim';
+  'ojroques/vim-oscyank';
+
+  'github/copilot.vim';
+
+  'gbprod/yanky.nvim';
+
+  'feline-nvim/feline.nvim';
+}
+
+require('lazy').setup(PKGS)
 
 vim.cmd [[ cnoreabbrev W w ]]
 vim.cmd [[ cnoreabbrev Wq wq ]]
@@ -47,7 +115,6 @@ vim.opt.wrap = true
 vim.opt.linebreak = true
 vim.opt.colorcolumn = "80"
 vim.opt.cursorline = true
-pcall(vim.cmd, "colorscheme kanagawa")
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
 

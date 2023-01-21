a8dfce8143f47e3f65ecaf9847921cae96741486
Rufo Sanchez <rufo@rufosanchez.com>
2023-01-21 11:46:28 -0600

    nvim: add nvim-navic and feline config

--- nvim/init.lua
+++ nvim/init.lua
@@ -46,6 +46,19 @@ local PKGS = {
       end
     end,
   },
+  {
+    "SmiteshP/nvim-navic",
+    lazy = true,
+    init = function()
+      vim.g.navic_silence = true
+      require("lsp-zero").on_attach(function(client, buffer)
+        if client.server_capabilities.documentSymbolProvider then
+          require("nvim-navic").attach(client, buffer)
+        end
+      end)
+    end,
+    opts = { separator = " ", highlight = true, depth_limit = 5 },
+  },
   {'rebelot/kanagawa.nvim',
     lazy = false,
     priority = 1000,
@@ -366,7 +379,31 @@ local PKGS = {
   };
 
   {'feline-nvim/feline.nvim',
-    config = true,
+    config = function ()
+      local components = require('feline.default_components').statusline.icons
+
+      local navic = require("nvim-navic")
+      table.insert(components.active[1], {
+        provider = function()
+          return navic.get_location()
+        end,
+        enabled = function()
+          return navic.is_available()
+        end,
+        left_sep = {
+          "  ",
+          { str = "slant_left",
+            hl = {
+              fg = 'fg',
+              bg = 'bg',
+            }
+          },
+          " ",
+        }
+      })
+      require('feline').setup({components = components})
+    end,
+    event = "VeryLazy",
   };
 }
 

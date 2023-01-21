e7948eea1b91c7101271cff90e6eb3695fbaf45b
Rufo Sanchez <rufo@rufosanchez.com>
2023-01-21 11:45:49 -0600

    nvim: Add dressing.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -30,6 +30,22 @@ local PKGS = {
       vim.g.startuptime_tries = 10
     end,
   },
+  {
+    "stevearc/dressing.nvim",
+    lazy = true,
+    init = function()
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.select = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.select(...)
+      end
+      ---@diagnostic disable-next-line: duplicate-set-field
+      vim.ui.input = function(...)
+        require("lazy").load({ plugins = { "dressing.nvim" } })
+        return vim.ui.input(...)
+      end
+    end,
+  },
   {'rebelot/kanagawa.nvim',
     lazy = false,
     priority = 1000,

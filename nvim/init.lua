5743d233baba1845d76116abbbd0277383fecc21
Rufo Sanchez <rufo@rufosanchez.com>
2023-01-21 11:45:04 -0600

    nvim: add startuptime plugin

--- nvim/init.lua
+++ nvim/init.lua
@@ -23,7 +23,13 @@ vim.opt.rtp:prepend(lazypath)
 local PKGS = {
   {'nvim-lua/plenary.nvim',
     lazy = true,
-  };
+  },
+  { "dstein64/vim-startuptime",
+    cmd = "StartupTime",
+    config = function()
+      vim.g.startuptime_tries = 10
+    end,
+  },
   {'rebelot/kanagawa.nvim',
     lazy = false,
     priority = 1000,

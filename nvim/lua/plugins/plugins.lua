a76571a7588df18cb84f7e79e3d6cec2667857d0
Rufo Sanchez <rufo@rufosanchez.com>
2023-02-18 12:43:18 -0600

    nvim: break out treesitter to separate file

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -34,57 +34,6 @@ return {
     end,
     opts = { separator = " ", highlight = true, depth_limit = 5 },
   },
-  {'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    event = "BufReadPost",
-    opts = {
-      ensure_installed = {
-        "ruby",
-        "lua",
-        "json",
-        "dockerfile",
-        "javascript",
-        "typescript",
-        "bash",
-        "comment",
-        "css",
-        "go",
-        "html",
-        "python",
-        "regex",
-        "scss",
-        "toml",
-        "vim",
-        "yaml",
-        "tsx",
-        "vue",
-        "make",
-        "c",
-        "markdown",
-        "markdown_inline",
-        "help",
-      },
-      -- Install parsers synchronously when headless
-      sync_install = #vim.api.nvim_list_uis() == 0,
-
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-
-      indent = {
-        enable = true,
-      },
-
-      endwise = {
-        enable = true,
-      },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-    end,
-    dependencies = {'RRethy/nvim-treesitter-endwise',},
-  };
   {'wincent/ferret',
     cmd = {
       "Ack",
@@ -320,11 +269,6 @@ return {
     },
     lazy = true,
   };
-
-  {'RRethy/nvim-treesitter-endwise',
-    lazy = true,
-  };
-
   {'lukas-reineke/indent-blankline.nvim',
     opts = {
       show_current_context = true,
---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,53 @@
+return {
+{'nvim-treesitter/nvim-treesitter',
+build = ':TSUpdate',
+event = "BufReadPost",
+opts = {
+  ensure_installed = {
+    "ruby",
+    "lua",
+    "json",
+    "dockerfile",
+    "javascript",
+    "typescript",
+    "bash",
+    "comment",
+    "css",
+    "go",
+    "html",
+    "python",
+    "regex",
+    "scss",
+    "toml",
+    "vim",
+    "yaml",
+    "tsx",
+    "vue",
+    "make",
+    "c",
+    "markdown",
+    "markdown_inline",
+    "help",
+  },
+  -- Install parsers synchronously when headless
+  sync_install = #vim.api.nvim_list_uis() == 0,
+
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+
+  indent = {
+    enable = true,
+  },
+
+  endwise = {
+    enable = true,
+  },
+},
+config = function(_, opts)
+  require("nvim-treesitter.configs").setup(opts)
+end,
+dependencies = {'RRethy/nvim-treesitter-endwise',},
+};
+}

2b81216646ba44f7af085b1f2e2c5facbd96b97a
Rufo Sanchez <rufo@rufosanchez.com>
2023-02-18 13:19:57 -0600

    nvim: move tree out into separate file

---
+++ nvim/lua/plugins/tree.lua
@@ -0,0 +1,29 @@
+return {
+  {'nvim-neo-tree/neo-tree.nvim',
+    opts = {
+      event_handlers = {
+        {
+          event = "file_opened",
+          handler = function(file_path)
+            require("neo-tree").close_all()
+          end
+        }
+      },
+      window = {
+        mappings = {
+          ["o"] = "open"
+        }
+      }
+    },
+    branch = "v2.x",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    keys = {
+      { "<leader>[", ":Neotree toggle=true<CR>", desc = "Neotree filesystem toggle" },
+      { "<leader>]", ":Neotree filesystem reveal<CR>", desc = "Find file in NvimTree" },
+    },
+    event = "BufEnter"
+  };
+}

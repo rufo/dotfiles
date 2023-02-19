bb7aeffe549369253aee8a16cd040304d9ef90b0
Rufo Sanchez <rufo@rufosanchez.com>
2023-02-18 20:00:45 -0600

    nvim: move mason into a separate file

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -38,26 +38,7 @@ return {
   {'tpope/vim-rails',
     event = "VeryLazy",
   };
-  {'williamboman/mason.nvim',
-    cmd = "Mason",
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "shellcheck",
-        "shfmt",
-      }
-    },
-    config = function(_, opts)
-      require("mason").setup(opts)
-      local mr = require("mason-registry")
-      for _, tool in ipairs(opts.ensure_installed) do
-        local p = mr.get_package(tool)
-        if not p:is_installed() then
-          p:install()
-        end
-      end
-    end,
-  };
+
   -- Snippets
 
 

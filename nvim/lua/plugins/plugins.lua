0d085674bc739563759e58958e863d03314b35e8
Rufo Sanchez <rufo@rufosanchez.com>
2023-02-18 19:57:31 -0600

    nvim: move search into a separate file

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -34,35 +34,9 @@ return {
     end,
     opts = { separator = " ", highlight = true, depth_limit = 5 },
   },
-  {'wincent/ferret',
-    cmd = {
-      "Ack",
-      "Lack",
-      "Back",
-      "Black",
-      "Quack",
-      "Acks",
-      "Lacks",
-      "Qargs",
-      "Largs",
-    }
-  };
 
   {'tpope/vim-rails',
     event = "VeryLazy",
-  };
-  {'junegunn/fzf',
-    build = './install --bin',
-    lazy = true,
-  };
-  {'ibhagwan/fzf-lua',
-    dependencies = {
-      "junegunn/fzf",
-    },
-    keys = {
-      { "<leader>p", "<cmd>lua require('fzf-lua').files()<CR>", desc = "FZF Files" },
-      { "<leader>b", "<cmd>lua require('fzf-lua').buffers()<CR>", desc = "FZF Buffers" },
-    }
   };
   {'williamboman/mason.nvim',
     cmd = "Mason",
---
+++ nvim/lua/plugins/search.lua
@@ -0,0 +1,28 @@
+return {
+  {'wincent/ferret',
+    cmd = {
+      "Ack",
+      "Lack",
+      "Back",
+      "Black",
+      "Quack",
+      "Acks",
+      "Lacks",
+      "Qargs",
+      "Largs",
+    }
+  };
+  {'junegunn/fzf',
+    build = './install --bin',
+    lazy = true,
+  };
+  {'ibhagwan/fzf-lua',
+    dependencies = {
+      "junegunn/fzf",
+    },
+    keys = {
+      { "<leader>p", "<cmd>lua require('fzf-lua').files()<CR>", desc = "FZF Files" },
+      { "<leader>b", "<cmd>lua require('fzf-lua').buffers()<CR>", desc = "FZF Buffers" },
+    }
+  };
+}

710bfa21545285c7723993652f5b571da0801bd9
Rufo Sanchez <rufo@rufosanchez.com>
2023-02-18 21:18:43 -0600

    nvim: move dressing.nvim into new ui file

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -5,22 +5,7 @@ return {
       vim.g.startuptime_tries = 10
     end,
   },
-  {
-    "stevearc/dressing.nvim",
-    lazy = true,
-    init = function()
-      ---@diagnostic disable-next-line: duplicate-set-field
-      vim.ui.select = function(...)
-        require("lazy").load({ plugins = { "dressing.nvim" } })
-        return vim.ui.select(...)
-      end
-      ---@diagnostic disable-next-line: duplicate-set-field
-      vim.ui.input = function(...)
-        require("lazy").load({ plugins = { "dressing.nvim" } })
-        return vim.ui.input(...)
-      end
-    end,
-  },
+
 
 
   -- Snippets

02118add8aa3bdef2df760ec663332a8d37c92e3
Rufo Sanchez <rufo@rufosanchez.com>
2023-02-26 21:40:42 -0600

    nvim: add autopairs back

--- nvim/lua/plugins/text-actions.lua
+++ nvim/lua/plugins/text-actions.lua
@@ -10,4 +10,18 @@ return {
     },
     event = 'BufReadPre',
   },
+  {
+    'windwp/nvim-autopairs',
+    config = function()
+      require('nvim-autopairs').setup()
+
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local cmp = require('cmp')
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done()
+      )
+    end,
+    event = 'InsertEnter',
+  }
 }

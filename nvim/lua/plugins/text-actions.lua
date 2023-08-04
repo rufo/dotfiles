60e9752ab1d5079dfd049c2273e26ceed3ecffd1
Rufo Sanchez <rufo@rufosanchez.com>
2023-08-04 16:24:16 -0500

    nvim: add ufo for folding

--- nvim/lua/plugins/text-actions.lua
+++ nvim/lua/plugins/text-actions.lua
@@ -23,5 +23,28 @@ return {
       )
     end,
     event = 'InsertEnter',
-  }
+  },
+  {
+    'kevinhwang91/nvim-ufo',
+    dependencies = {
+      'kevinhwang91/promise-async',
+    },
+    config = function ()
+      vim.o.foldcolumn = '1'
+      vim.o.foldlevel = 99
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
+      require('ufo').setup({
+        provider_selector = function (bufnr, filetype, buftype)
+          return {'treesitter', 'indent'}
+        end
+      })
+    end,
+    event = "BufReadPost",
+    keys = {
+      {'zR', function () require('ufo').openAllFolds() end},
+      {'zM', function () require('ufo').closeAllFolds() end},
+    },
+  },
 }

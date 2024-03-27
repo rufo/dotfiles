dded2a21f82ffaa728ecf365c3b306b3899ade69
Rufo Sanchez <rufo@rufosanchez.com>
2024-03-27 17:09:58 -0500

    nvim: add vim-rhubarb (why wasn't it here before now exactly?)

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -1,4 +1,5 @@
 return {
   { 'lewis6991/gitsigns.nvim', config = true, event = 'BufReadPre' },
   { 'tpope/vim-fugitive', event = 'VeryLazy' },
+  { 'tpope/vim-rhubarb', event = 'VeryLazy', dependencies = 'vim-fugitive' },
 }

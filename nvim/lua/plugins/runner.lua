4ab6814a4be8a8d9fc05f60b93fb7b43d0fc8c1a
Rufo Sanchez <rufo@rufosanchez.com>
2024-10-07 12:37:04 -0500

    nvim: add sniprunner

---
+++ nvim/lua/plugins/runner.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'michaelb/sniprun',
+    build = "sh ./install.sh",
+    cmd = {
+      'SnipRun',
+      'SnipReset',
+      'SnipClose',
+      'SnipInfo',
+      'SnipReplMemoryClean',
+      'SnipRunOperator',
+    },
+  }
+}

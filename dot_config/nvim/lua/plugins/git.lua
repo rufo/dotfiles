fb4a5c4d5424b40d9f03dd787356f274dd4f9a9d
Rufo Sanchez <rufo@rufosanchez.com>
2025-05-26 10:30:06 -0500

    nvim: remove lazy loading for gitsigns

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -1,5 +1,5 @@
 return {
-  { 'lewis6991/gitsigns.nvim', config = true, event = 'BufReadPre' },
+  { 'lewis6991/gitsigns.nvim', config = true },
   { 'tpope/vim-fugitive', event = 'VeryLazy' },
   { 'tpope/vim-rhubarb', event = 'VeryLazy', dependencies = 'vim-fugitive' },
 }

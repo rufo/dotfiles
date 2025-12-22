3041571466029f145e7c20f8c2ea307ac95a469f
Rufo Sanchez <rufo@rufosanchez.com>
2025-12-21 22:50:54 -0600

    neovim: update treesitter configuration

--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -2,11 +2,7 @@ return {
   {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-    init = function(plugin)
-      require("lazy.core.loader").add_to_rtp(plugin)
-      require("nvim-treesitter.query_predicates")
-    end,
+    lazy = false,
     opts = {
       ensure_installed = {
         'ruby',
@@ -36,24 +32,21 @@ return {
       },
       -- Install parsers synchronously when headless
       sync_install = #vim.api.nvim_list_uis() == 0,
-
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-
-      indent = {
-        enable = true,
-      },
-
-      endwise = {
-        enable = true,
-      },
+    -- TODO: these might have different ways to enable now?
+    --   highlight = {
+    --     enable = true,
+    --     additional_vim_regex_highlighting = false,
+    --   },
+    --
+    --   indent = {
+    --     enable = true,
+    --   },
     },
-    config = function(_, opts)
-      require('nvim-treesitter.configs').setup(opts)
-    end,
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-    dependencies = { 'RRethy/nvim-treesitter-endwise' },
+  },
+  {
+    'RRethy/nvim-treesitter-endwise',
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter"
+    }
   },
 }
